b7337c4684fbf1d21048012ee0f25f350ef6c729
mahnoor-shakil <mah.realone@gmail.com>
2024-03-30 23:09:37 +0500

    add one html and one app.js file

---
+++ app.js
@@ -0,0 +1,28 @@
+// q#1
+// var userinput = +prompt ("enter number")
+// document.write ("number: " + userinput)
+// document.write ("round off value: " (Math.round(userinput)));
+// document.write ("floor value: " (Math.floor(userinput)));
+// document.write ("ceil value: " (Math.ceil(userinput)));
+
+// // q#2
+// var number = +prompt ("enter value")
+// var roundOff = Math.round(number);
+// var floorValue = Math.floor(number);
+// var ceilValue = Math.ceil(number);
+  
+// var resultString = "Number: " + number + "<br>";
+// resultString += "Round off value: " + roundOff + "<br>";
+// resultString += "Floor value: " + floorValue + "<br>";
+// resultString += "Ceil value: " + ceilValue + "<br>";
+
+// document.write (resultString)
+
+// q3
+// var number = +prompt("enter value")
+// var absoluteValue = Math.abs(number);
+// document.write("the absolute value of -4 is " + number)
+
+// q4
+var diceValue = Math.floor(Math.random() * 6) + 1;
+document.write("random dice value: " + diceValue)
